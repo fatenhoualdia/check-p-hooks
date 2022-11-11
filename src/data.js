771a64a14946f857de2cbf9486930b250657c609
fatenhoualdia <113390735+fatenhoualdia@users.noreply.github.com>
2022-11-11 05:55:19 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,42 +1,32 @@
-export const movieData = [
-    {title:"c",
-    description:"In April 1986, an explosion at the Chernobyl nuclear power plant in the Union of Soviet Socialist Republics becomes one of the world's worst man-made catastrophes",
-    posterURL:"https://m.media-amazon.com/images/I/711BIyFlWUL._SS500_.jpg",
-    rate:4},
-
-    {title:"Rick & Morty",
-    description:"An animated series that follows the exploits of a super scientist and his not-so-bright grandson",
-    posterURL:"https://fr.web.img6.acsta.net/pictures/18/10/31/17/34/2348073.jpg",
-    rate:4},
+export const dataMovies = [
     {
-      title:"Game of Thrones",
-    description:"Nine noble families fight for control over the lands of Westeros, while an ancient enemy returns after being dormant for millennia ",
-    posterURL:"https://celebrity.fm/wp-content/uploads/2020/08/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_.jpg",
-    rate:5
+        "id": 2,
+        "title": "Final Fantasy",
+        "description": "A scientist makes a last stand on Earth with the help of a ragtag team of soldiers against an invasion of alien phantoms.",
+        "posterUrl": "https://upload.wikimedia.org/wikipedia/en/3/34/Final_Fantasy_The_Spirits_Within_%282011_film%29_poster.jpg",
+        "rating": 1,
+        "trailerSrc": "https://www.youtube.com/embed/4tyuIh12_HU"
     },
     {
-      title:"The Queens Gambit",
-    description:"The Queen's Gambit is a 2020 American coming-of-age period drama miniseries based on Walter Tevis's 1983 novel of the same name.",
-    posterURL:"https://upload.wikimedia.org/wikipedia/en/1/12/The_Queen%27s_Gambit_%28miniseries%29.png",
-    rate:3
+        "id": 3,
+        "title": "Ghost In The Shell",
+        "description": "A hacker known as the Puppet Master is hunted by a female cyborg cop and her partner. This film is a revised version of Ghost in the Shell (1995).",
+        "posterUrl": "https://fr.web.img4.acsta.net/pictures/17/03/01/17/45/314818.jpg",
+        "rating": 5,
+        "trailerSrc": "https://www.youtube.com/embed/G4VmJcZR0Yg"
     },
-    
-   
-
-    {title:"Sherlock",
-    description:"A modern update finds the famous sleuth and his doctor partner solving crime in 21st century London.",
-    posterURL:"https://fr.web.img5.acsta.net/pictures/18/11/05/18/04/4981046.jpg",
-    rate:2},
-
-    {title:"Death Note",
-    description:"An intelligent high school student goes on a secret crusade to eliminate criminals from the world after discovering a notebook capable of killing anyone whose name is written into it.",
-    posterURL:"https://fr.web.img5.acsta.net/pictures/18/01/18/14/35/2024405.jpg",
-    rate:4},
-
     {
-      title: "Peaky Blinders",
-      description: "A gangster family epic set in 1900s England, centering on a gang who sew razor blades in the peaks of their caps, and their fierce boss Tommy Shelby",
-      posterURL: "https://fr.web.img6.acsta.net/pictures/210/457/21045721_20131001172258551.jpg",
-      rate:5
-    }
-  ];
+        "id": 4,
+        "title": "Appleseed Alpha",
+        "description": "A young female soldier Deunan and her cyborg partner Briareos survive through the post World War 3 apocalyptic New York in search of human's future hope.",
+        "posterUrl": "https://m.media-amazon.com/images/M/MV5BMTgzMDY2Njc4Nl5BMl5BanBnXkFtZTgwNTEyMjUzMjE@._V1_FMjpg_UX1000_.jpg",
+        "rating": 4,
+        "trailerSrc": "https://www.youtube.com/embed/4DtWCjogd1M"
+    },
+    {
+        "id": 5,
+        "title": "Resident Evil",
+        "description": "Chris Redfield enlists the help of Leon S. Kennedy and Rebecca Chambers to stop a death merchant, with a vengeance, from spreading a deadly virus in New York.",
+        "posterUrl": "https://upload.wikimedia.org/wikipedia/en/a/a1/Resident_evil_ver4.jpg",
+        "rating": 2,
+        "trailerSrc": "https://www.youtube.com/embed/W1OUs3HwIuo"
